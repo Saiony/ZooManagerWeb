6321d3460967cc3744eb76b0ba70a827be326c02
Cauê <cauechavesvicente@gmail.com>
2026-06-28 11:31:36 -0300

    project setup

---
+++ ZooManager/src/main.ts
@@ -0,0 +1,31 @@
+import { Application, Assets, Sprite } from "pixi.js";
+
+(async () => 
+{
+  
+  const app = new Application();
+  await app.init({ background: "#1099bb", resizeTo: window });
+
+  // Append the application canvas to the document body
+  document.getElementById("pixi-container")!.appendChild(app.canvas);
+
+  // Load the bunny texture
+  const texture = await Assets.load("/assets/bunny.png");
+
+  // Create a bunny Sprite
+  const bunny = new Sprite(texture);
+
+  // Center the sprite's anchor point
+  bunny.anchor.set(0.5);
+
+  // Move the sprite to the center of the screen
+  bunny.position.set(app.screen.width / 2, app.screen.height / 2);
+
+  // Add the bunny to the stage
+  app.stage.addChild(bunny);
+
+  app.ticker.add((time) => 
+  {
+    bunny.rotation += 0.1 * time.deltaTime;
+  });
+})();
